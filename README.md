93cd30263fad2e808e103e5de62c3456211bf447
Valentin <libouton.v@proton.me>
2023-05-23 14:26:53 +0200

    version 1 fonctionnelle

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# find_duplicates
+# Find Duplicates
+
+Find Duplicates is a command-line tool for searching and identifying duplicate files within a directory. It compares files based on their name and size and provides options to display the duplicate files or generate output in various formats.
+
+## Installation
+
+1. Clone the repository:
+```bash
+git clone git@github.com:ValentinLibouton/find_duplicates.git
+```
+2. Navigate to the project directory:
+```bash
+cd find_duplicates
+```
+3. Install the required dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the script `find_duplicates.py` with the following command-line arguments:
+```bash
+python find_duplicates.py directory [output_type] [--duplicates]
+```
+- `directory` (required): The directory to search for duplicate files.
+- `output_type` (optional): The type of output. Supported options are "terminal", "html", and "csv". The default is "terminal".
+- `--duplicates` (optional): Display only duplicate files. If specified, the script will only show duplicate files; otherwise, it will display all files in the directory.
+
+Examples:
+
+- Search for duplicate files in a directory and display the results in the terminal:
+```bash
+python find_duplicates.py /path/to/directory
+```
+- Search for duplicate files in a directory and generate an HTML report:
+```bash
+python find_duplicates.py /path/to/directory html
+```
+- Search for duplicate files in a directory and generate a CSV file:
+```bash
+python find_duplicates.py /path/to/directory csv
+```
+- Search for duplicate files only and display the results in the terminal:
+```bash
+python find_duplicates.py /path/to/directory terminal --duplicates
+```
+
+## License
+
+- This project is licensed under **...**
---
+++ find_duplicates.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import argparse
+import datetime
+import csv
+from collections import Counter
+from itertools import groupby
+
+def get_file_info(file_path):
+    file_name = os.path.basename(file_path)
+    modified_time = datetime.datetime.fromtimestamp(os.path.getmtime(file_path))
+    file_size = os.path.getsize(file_path)
+    absolute_path = os.path.abspath(file_path)
+    return [file_name, modified_time, file_size, absolute_path]
+
+def search_files(directory):
+    file_list = []
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_info = get_file_info(file_path)
+            file_list.append(file_info)
+    return file_list
+
+def sort_files(file_list):
+    sorted_files = sorted(file_list, key=lambda x: (x[0][0], x[0][1], x[0][2]) if x else [])
+    return sorted_files
+
+def output_to_terminal(file_list):
+    for file_info in file_list:
+        print(file_info)
+
+def output_to_csv(file_list, output_file):
+    with open(output_file, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['File Name', 'Modified Time', 'File Size', 'Absolute Path'])
+        for file_info in file_list:
+            writer.writerow(file_info)
+
+def output_to_html(file_list, output_file):
+    with open(output_file, 'w') as html_file:
+        html_file.write('<html>\n')
+        html_file.write('<head>\n')
+        html_file.write('<title>File List</title>\n')
+        html_file.write('</head>\n')
+        html_file.write('<body>\n')
+        html_file.write('<table>\n')
+        html_file.write('<tr><th>File Name</th><th>Modified Time</th><th>File Size</th><th>Absolute Path</th></tr>\n')
+        for file_info in file_list:
+            html_file.write('<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>\n'.format(file_info[0], file_info[1], file_info[2], file_info[3]))
+        html_file.write('</table>\n')
+        html_file.write('</body>\n')
+        html_file.write('</html>\n')
+
+def get_duplicate_files(file_list):
+    """
+    Get a list of duplicate files based on file name and size.
+
+    Args:
+        file_list (list): List of files.
+
+    Returns:
+        list: List of duplicate files, where each file is represented as a list
+        containing the file name, modified time, file size, and absolute path.
+    """
+    file_list.sort(key=lambda x: (x[0], x[2]))  # Sort the files by name and size
+    duplicate_files = []
+    for key, group in groupby(file_list, key=lambda x: (x[0], x[2])):
+        files = list(group)
+        if len(files) > 1:
+            duplicate_files.extend(files)
+    return duplicate_files
+
+def main():
+    parser = argparse.ArgumentParser(description='Recursive file search script')
+    parser.add_argument('directory', help='The directory to search in')
+    parser.add_argument('output_type', nargs='?', default='terminal', choices=['terminal', 'html', 'csv'], 
+                        help='The type of output (terminal, html, csv)')
+    parser.add_argument('--duplicates', action='store_true', help='Display only duplicate files')
+    args = parser.parse_args()
+
+    if not args.directory:
+        parser.error('The directory argument is required.')
+
+    file_list = search_files(args.directory)
+
+    if args.duplicates:
+        file_list = get_duplicate_files(file_list)
+
+    sorted_files = sort_files(file_list)
+
+    if args.output_type == 'terminal':
+        output_to_terminal(sorted_files)
+    elif args.output_type == 'html':
+        output_to_html(sorted_files, 'output.html')
+    elif args.output_type == 'csv':
+        output_to_csv(sorted_files, 'output.csv')
+
+if __name__ == '__main__':
+    main()
